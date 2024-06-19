67abff45ba2ec7f97cf76c6b1f357cf66ce58517
Erkan Kaya <138982167+eerkankaaya@users.noreply.github.com>
2024-06-19 03:47:29 +0300

    Add files via upload

---
+++ AdoNetDemo/AdoNetDemo/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace AdoNetDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgwProducts = new System.Windows.Forms.DataGridView();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.tbxName = new System.Windows.Forms.TextBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblUnitPrice = new System.Windows.Forms.Label();
+            this.lblStockAmount = new System.Windows.Forms.Label();
+            this.tbxUnitPrice = new System.Windows.Forms.TextBox();
+            this.tbxStockAmount = new System.Windows.Forms.TextBox();
+            this.gbxAdd = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgwProducts)).BeginInit();
+            this.gbxAdd.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dgwProducts
+            // 
+            this.dgwProducts.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgwProducts.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgwProducts.Location = new System.Drawing.Point(158, 99);
+            this.dgwProducts.Name = "dgwProducts";
+            this.dgwProducts.Size = new System.Drawing.Size(691, 177);
+            this.dgwProducts.TabIndex = 0;
+            this.dgwProducts.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(141, 142);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // tbxName
+            // 
+            this.tbxName.Location = new System.Drawing.Point(141, 18);
+            this.tbxName.Name = "tbxName";
+            this.tbxName.Size = new System.Drawing.Size(100, 20);
+            this.tbxName.TabIndex = 2;
+            this.tbxName.TextChanged += new System.EventHandler(this.tbxName_TextChanged);
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(18, 25);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(35, 13);
+            this.lblName.TabIndex = 3;
+            this.lblName.Text = "Name";
+            this.lblName.Click += new System.EventHandler(this.lblName_Click);
+            // 
+            // lblUnitPrice
+            // 
+            this.lblUnitPrice.AutoSize = true;
+            this.lblUnitPrice.Location = new System.Drawing.Point(18, 68);
+            this.lblUnitPrice.Name = "lblUnitPrice";
+            this.lblUnitPrice.Size = new System.Drawing.Size(53, 13);
+            this.lblUnitPrice.TabIndex = 4;
+            this.lblUnitPrice.Text = "Unit Price";
+            this.lblUnitPrice.Click += new System.EventHandler(this.lblUnitPrice_Click);
+            // 
+            // lblStockAmount
+            // 
+            this.lblStockAmount.AutoSize = true;
+            this.lblStockAmount.Location = new System.Drawing.Point(18, 105);
+            this.lblStockAmount.Name = "lblStockAmount";
+            this.lblStockAmount.Size = new System.Drawing.Size(74, 13);
+            this.lblStockAmount.TabIndex = 5;
+            this.lblStockAmount.Text = "Stock Amount";
+            this.lblStockAmount.Click += new System.EventHandler(this.lblStockAmount_Click);
+            // 
+            // tbxUnitPrice
+            // 
+            this.tbxUnitPrice.Location = new System.Drawing.Point(141, 68);
+            this.tbxUnitPrice.Name = "tbxUnitPrice";
+            this.tbxUnitPrice.Size = new System.Drawing.Size(100, 20);
+            this.tbxUnitPrice.TabIndex = 6;
+            this.tbxUnitPrice.TextChanged += new System.EventHandler(this.tbxUnitPrice_TextChanged);
+            // 
+            // tbxStockAmount
+            // 
+            this.tbxStockAmount.Location = new System.Drawing.Point(141, 105);
+            this.tbxStockAmount.Name = "tbxStockAmount";
+            this.tbxStockAmount.Size = new System.Drawing.Size(100, 20);
+            this.tbxStockAmount.TabIndex = 7;
+            this.tbxStockAmount.TextChanged += new System.EventHandler(this.tbxStockAmount_TextChanged);
+            // 
+            // gbxAdd
+            // 
+            this.gbxAdd.Controls.Add(this.lblName);
+            this.gbxAdd.Controls.Add(this.tbxStockAmount);
+            this.gbxAdd.Controls.Add(this.btnAdd);
+            this.gbxAdd.Controls.Add(this.tbxUnitPrice);
+            this.gbxAdd.Controls.Add(this.tbxName);
+            this.gbxAdd.Controls.Add(this.lblStockAmount);
+            this.gbxAdd.Controls.Add(this.lblUnitPrice);
+            this.gbxAdd.Location = new System.Drawing.Point(340, 356);
+            this.gbxAdd.Name = "gbxAdd";
+            this.gbxAdd.Size = new System.Drawing.Size(316, 168);
+            this.gbxAdd.TabIndex = 8;
+            this.gbxAdd.TabStop = false;
+            this.gbxAdd.Text = "Add a product";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1486, 780);
+            this.Controls.Add(this.gbxAdd);
+            this.Controls.Add(this.dgwProducts);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgwProducts)).EndInit();
+            this.gbxAdd.ResumeLayout(false);
+            this.gbxAdd.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgwProducts;
+        private System.Windows.Forms.GroupBox gbxAdd;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblUnitPrice;
+        private System.Windows.Forms.Label lblStockAmount;
+        private System.Windows.Forms.TextBox tbxName;
+        private System.Windows.Forms.TextBox tbxUnitPrice;
+        private System.Windows.Forms.TextBox tbxStockAmount;
+        private System.Windows.Forms.Button btnAdd;
+
+
+
+
+    }
+}
+
---
+++ AdoNetDemo/AdoNetDemo/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdoNetDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+        ProductDal _productDal = new ProductDal();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+           
+
+            dgwProducts.DataSource = _productDal.GelAll();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            _productDal.Add(new Product
+            {
+                Name = tbxName.Text,
+                UnitPrice = Convert.ToDecimal(tbxUnitPrice.Text),
+                StockAmount = Convert.ToInt32(tbxStockAmount.Text),
+            });
+            MessageBox.Show("Product Added!");
+        }
+
+        private void tbxStockAmount_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbxUnitPrice_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbxName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblName_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblUnitPrice_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblStockAmount_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
